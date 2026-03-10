1e4ccee9fabb0718cbcc3b7c8106910579728377
Adriano Regis <adbrum@outlook.com>
2026-03-10 17:50:22 +0000

    Update login functionality, API service integration, and environment configurations.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,38 @@
 import type { NextConfig } from "next";
 
+const BACKEND_URL =
+  process.env.NEXT_PUBLIC_API_BASE?.replace("/api/1", "") || "http://localhost:7000";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/logout/",
+          destination: `${BACKEND_URL}/logout/`,
+        },
+        {
+          source: "/get-csrf",
+          destination: `${BACKEND_URL}/get-csrf`,
+        },
+        {
+          source: "/reset/",
+          destination: `${BACKEND_URL}/reset/`,
+        },
+        {
+          source: "/reset/:token",
+          destination: `${BACKEND_URL}/reset/:token`,
+        },
+      ],
+      afterFiles: [],
+      fallback: [
+        {
+          source: "/api/:path*",
+          destination: `${BACKEND_URL}/api/:path*`,
+        },
+      ],
+    };
+  },
 };
 
 export default nextConfig;
